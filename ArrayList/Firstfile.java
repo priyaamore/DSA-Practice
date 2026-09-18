ed3d7fde68e810cc4e157bcbe163da17425c4e26
Priya  More <priyamore8446@gmail.com>
2026-09-18 15:50:46 +0530

    Add Firstfile.java with basic ArrayList example

---
+++ ArrayList/Firstfile.java
@@ -0,0 +1,11 @@
+package ArrayList;
+
+import java.util.*;
+public class Firstfile {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+    }
+}
